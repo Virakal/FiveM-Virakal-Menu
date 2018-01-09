b85e30c54ec354c95ea965edccd9b9face4d5ead
Jon Goodger <jonno.is@gmail.com>
2018-01-09 00:23:32 +0000

    Make sections into scripts of their own

--- TrainerClient/Section/BaseSection.cs
+++ TrainerClient/Section/BaseSection.cs
@@ -1,11 +1,13 @@
-﻿namespace Virakal.FiveM.Trainer.TrainerClient.Section
+﻿using CitizenFX.Core;
+
+namespace Virakal.FiveM.Trainer.TrainerClient.Section
 {
-    abstract class BaseSection
+    abstract class BaseSection : BaseScript
     {
         protected Trainer Trainer { get; }
         protected Config Config { get; }
 
-        public BaseSection(Trainer trainer)
+        public BaseSection(Trainer trainer) : base()
         {
             Trainer = trainer;
             Config = trainer.Config;
