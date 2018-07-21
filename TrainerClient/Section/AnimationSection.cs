1a1e8cc0a6edb145bf9d02066ad724b7397e44fa
Jon Goodger <jonno.is@gmail.com>
2018-07-22 00:56:07 +0100

    Add cow bomb in a stupid place

--- TrainerClient/Section/AnimationSection.cs
+++ TrainerClient/Section/AnimationSection.cs
@@ -13,6 +13,46 @@ namespace Virakal.FiveM.Trainer.TrainerClient.Section
         public AnimationSection(Trainer trainer) : base(trainer)
         {
             Trainer.RegisterAsyncNUICallback("animate", OnAnimate);
+            Trainer.RegisterAsyncNUICallback("cowbomb", OnCowBomb);
+        }
+
+        private async Task<CallbackDelegate> OnCowBomb(IDictionary<string, object> data, CallbackDelegate callback)
+        {
+            callback("ok");
+
+            var rand = new Random();
+            var cowModel = new Model(@"A_C_Cow");
+
+            Debug.WriteLine("Loading cow model");
+
+            cowModel.Request();
+
+            while (!cowModel.IsLoaded)
+            {
+                Debug.WriteLine("Waiting...");
+                await BaseScript.Delay(1);
+            }
+
+            Debug.WriteLine("Loaded.");
+
+            for (var i = 0; i < 10; ++i)
+            {
+                var position = Game.PlayerPed.Position;
+                var heading = (float)rand.NextDouble();
+
+                position.X += (5 - rand.Next(10)) / 10;
+                position.Y += (5 - rand.Next(10)) / 10;
+                position.Z += 20 + (5 * (float)rand.NextDouble());
+
+                Debug.WriteLine($"Dropping cow #{1 + i} at {position}");
+
+                // Stagger spawns
+                await BaseScript.Delay(rand.Next(15));
+
+                var cow = await World.CreatePed(cowModel, position, heading);
+            }
+
+            return callback;
         }
 
         private async Task<CallbackDelegate> OnAnimate(IDictionary<string, object> data, CallbackDelegate callback)
