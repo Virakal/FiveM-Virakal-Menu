7d7913aaec4c05c93da28f070be624dc13378e3e
Jon Goodger <jonno.is@gmail.com>
2019-01-22 00:52:52 +0000

    Beginning of menu rewrite in Vue

--- TrainerShared/static/__resource.lua
+++ TrainerShared/static/__resource.lua
@@ -1,12 +1,13 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
-ui_page "nui/trainer.html"
-
-server_script '@mysql-async/lib/MySQL.lua'
+-- ui_page "nui/trainer.html"
+ui_page "nui/new-trainer.htm"
 
 files {
-	"nui/trainer.html",
-	"nui/trainer.js",
+	-- "nui/trainer.html",
+	"nui/new-trainer.htm",
+	-- "nui/trainer.js",
+	"nui/new-trainer.js",
 	"nui/trainer.css",
 	"nui/Roboto.ttf",
 	"Newtonsoft.Json.dll"
---
+++ TrainerUI/new-trainer.ts
@@ -0,0 +1,79 @@
+﻿/// <reference path="./js_refs/index.d.ts" />
+/// <reference path="./js_refs/jquery.d.ts" />
+//// <reference path="./node_modules/vue/types/vue.d.ts" />
+
+declare var Vue: any;
+const resourceName: string = 'virakal-trainer';
+
+function sendData(name: string, data: any): JQueryXHR {
+	return $.post(`http://${resourceName}/${name}`, JSON.stringify(data), function (response) {
+		// console.log('Data response: ' + response);
+	});
+}
+
+function playSound(sound: string): void {
+	sendData('playsound', { name: sound });
+}
+
+window.addEventListener('message', function (event) {
+	let item = event.data;
+
+	if (item.showtrainer) {
+		app.showTrainer = true;
+	} else if (item.hidetrainer) {
+		app.showTrainer = false;
+	}
+});
+
+Vue.component('trainer-option', {
+	props: ['text', 'sub', 'action', 'state'],
+	template: '<p class="traineroption" :data-sub="sub" :data-action="action" :data-state="state"><slot></slot></p>',
+});
+
+let mainMenu = [
+	{
+        text: "Player",
+		sub: "playermenu",
+	},
+	{
+        text: "Teleport",
+		sub: "teleportmenu",
+	},
+	{
+        text: "Vehicles",
+		sub: "vehiclesmenu",
+	},
+	{
+        text: "Weapons",
+		sub: "weaponsmenu",
+	},
+	{
+        text: "Police",
+		sub: "policemenu",
+	},
+	{
+        text: "Settings",
+		sub: "settingsmenu",
+	},
+	{
+        text: "Animate",
+		sub: "animationmenu",
+	},
+	{
+        text: "UI",
+		sub: "uimenu",
+	},
+	{
+        text: "Animal Bombs",
+		sub: "animalbombmenu",
+	},
+];
+
+let app = new Vue({
+	el: '#trainercontainer',
+	data: {
+		trainerTitle: 'Virakal Trainer',
+		showTrainer: false,
+		page: mainMenu
+	}
+});
