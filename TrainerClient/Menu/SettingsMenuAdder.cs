4d41ad46de5c5d7e6bc38f7a03c097e82589819e
Jon Goodger <jonno.is@gmail.com>
2019-01-25 23:55:23 +0000

    Generate the time menu automatically

--- TrainerClient/Menu/SettingsMenuAdder.cs
+++ TrainerClient/Menu/SettingsMenuAdder.cs
@@ -25,10 +25,15 @@ namespace Virakal.FiveM.Trainer.TrainerClient.Menu
                 },
             };
 
-            // We can build this from the enum in future
             menus["settings.weather"] = GetWeatherMenu();
+            menus["settings.time"] = GetTimeMenu();
 
-            menus["settings.time"] = new List<MenuItem>()
+            return menus;
+        }
+
+        private List<MenuItem> GetTimeMenu()
+        {
+            var list = new List<MenuItem>()
             {
                 new MenuItem()
                 {
@@ -50,129 +55,18 @@ namespace Virakal.FiveM.Trainer.TrainerClient.Menu
                     text = "Midnight",
                     action = "time 0"
                 },
-                new MenuItem()
-                {
-                    text = "00:00",
-                    action = "time 0"
-                },
-                new MenuItem()
-                {
-                    text = "01:00",
-                    action = "time 1"
-                },
-                new MenuItem()
-                {
-                    text = "02:00",
-                    action = "time 2"
-                },
-                new MenuItem()
-                {
-                    text = "03:00",
-                    action = "time 3"
-                },
-                new MenuItem()
-                {
-                    text = "04:00",
-                    action = "time 4"
-                },
-                new MenuItem()
-                {
-                    text = "05:00",
-                    action = "time 5"
-                },
-                new MenuItem()
-                {
-                    text = "06:00",
-                    action = "time 6"
-                },
-                new MenuItem()
-                {
-                    text = "07:00",
-                    action = "time 7"
-                },
-                new MenuItem()
-                {
-                    text = "08:00",
-                    action = "time 8"
-                },
-                new MenuItem()
-                {
-                    text = "09:00",
-                    action = "time 9"
-                },
-                new MenuItem()
-                {
-                    text = "10:00",
-                    action = "time 10"
-                },
-                new MenuItem()
-                {
-                    text = "11:00",
-                    action = "time 11"
-                },
-                new MenuItem()
-                {
-                    text = "12:00",
-                    action = "time 12"
-                },
-                new MenuItem()
-                {
-                    text = "13:00",
-                    action = "time 13"
-                },
-                new MenuItem()
-                {
-                    text = "14:00",
-                    action = "time 14"
-                },
-                new MenuItem()
-                {
-                    text = "15:00",
-                    action = "time 15"
-                },
-                new MenuItem()
-                {
-                    text = "16:00",
-                    action = "time 16"
-                },
-                new MenuItem()
-                {
-                    text = "17:00",
-                    action = "time 17"
-                },
-                new MenuItem()
-                {
-                    text = "18:00",
-                    action = "time 18"
-                },
-                new MenuItem()
-                {
-                    text = "19:00",
-                    action = "time 19"
-                },
-                new MenuItem()
-                {
-                    text = "20:00",
-                    action = "time 20"
-                },
-                new MenuItem()
-                {
-                    text = "21:00",
-                    action = "time 21"
-                },
-                new MenuItem()
-                {
-                    text = "22:00",
-                    action = "time 22"
-                },
-                new MenuItem()
-                {
-                    text = "23:00",
-                    action = "time 23"
-                },
             };
 
-            return menus;
+            for (int i = 0; i < 24; i++)
+            {
+                list.Add(new MenuItem()
+                {
+                    text = $"{i:D2}:00",
+                    action = $"time {i}"
+                });
+            }
+
+            return list;
         }
 
         private List<MenuItem> GetWeatherMenu()
